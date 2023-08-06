66712dcb43c224e1eee71a92b17f92f4ff3f751f
Jerry Vai <125141044+jerryvai@users.noreply.github.com>
2023-08-06 21:00:06 +0530

    Add files via upload
    Signed-off-by: Jerry Vai <125141044+jerryvai@users.noreply.github.com>

---
+++ library/student/message.php
@@ -0,0 +1,37 @@
+<?php
+  include "connection.php";
+  include "navbar.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Message</title>
+</head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<style type="text/css">
+	body
+	{
+		
+		background-image: url("image/p.jpg"); 
+		background-repeat: no-repeat;
+	}
+.wrapper
+{
+	height: 600px;
+	width: 500px;
+	background-color: black;
+	opacity: .9;
+	color: white;
+	margin: -20px auto;
+	padding: 10px;
+}	
+</style>
+  
+<body>
+<div class="wrapper">
+	
+</div>	
+ 
+
+</body>
+</html>
